7396c1822fda8511634bc4c4a44eb58a063fcfa5
Axel SENZEL <axel.senzel@viacesi.fr>
2015-09-02 21:14:49 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import fr.ase.cow.HelloWorld;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HelloWorld hello = new HelloWorld();
+        hello.helloWorld();
+    }
+}
---
+++ src/fr/ase/cow/HelloWorld.java
@@ -0,0 +1,24 @@
+package fr.ase.cow;
+
+/**
+ * Permet de faire un hello world
+ *
+ * @author Axel Senzel
+ * @version 1.0.0
+ * @since 02/09/2015
+ */
+public class HelloWorld {
+
+    /**
+     * Constructeur par default
+     */
+    public HelloWorld() {
+    }
+
+    /**
+     * Permet de faire un hello world
+     */
+    public void helloWorld() {
+        System.out.println("yolo");
+    }
+}
